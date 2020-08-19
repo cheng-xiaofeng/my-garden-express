bcdbd4ddafa6bb5e0683bff7b0554a82f7502d24
xiaofeng.cheng <xiaofeng.cheng@keymantech.cn>
2020-08-19 16:00:06 +0800

    添加白名单

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Origin",'http://localhost:3001' || 'http://localhost:3000');
   //允许的header类型
-  res.header("Access-Control-Allow-Headers","content-type");
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization");
   //跨域允许的请求方式 
   res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
-  console.log(111);
+  res.header('Access-Control-Allow-Credentials', true)
+  console.log(req);
   
   res.send("how are you")
 });
